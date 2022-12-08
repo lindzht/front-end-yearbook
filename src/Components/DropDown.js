807ed7ad0663ee0b16a6d7db5e3f3bf1af1437c8
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-12-08 17:20:35 -0500

    Added id routing for cohorts.

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const DropDown = ({ cohortData, setCohortIndex, history }) => {
   const optionElements = cohortData.map((cohortData) => {
     return (
@@ -11,21 +10,19 @@ const DropDown = ({ cohortData, setCohortIndex, history }) => {
     );
   });
 
-
   const handleChange = (e) => {
     const index = e.target.value;
-    setCohortIndex(index);
+    setCohortIndex(index); 
+
 
     if (index > 0) {
-      history.push(`/cohorts`);
+      history.push(`/cohorts/${e.target.value}`)
     } else {
       history.push(`/`);
-
     }
   };
 
   return (
-
     <div id="cohort-drop-down">
       <label htmlFor="cohorts">
         <select onChange={handleChange} name="cohorts" id="cohorts">
