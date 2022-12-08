bd0f6bb8a6e555cca4acd54a22682c1d68a3655e
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-12-08 15:00:53 -0500

    added a mouse button. added a little bit of routing. Added a footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
+const Footer = ({ setCohortIndex }) => {
+  // const cohortList = cohorts.map((cohort) => {
+  //     return( <p>{cohort.region}: {cohort.start_date}</p>)
+  // })
 
+  const history = useHistory();
+  const handleGoBack = () => {
+    setCohortIndex(1);
+    history.goBack();
+  };
 
-const Footer =({cohorts})=> {
-
-    const cohortList = cohorts.map((cohort) => {
-        return( <p>{cohort.region}: {cohort.start_date}</p>)
-    })
-
-    return(
-        <div id='footer'>
-            <h1>Footer!</h1>
-            {cohortList}
-
-        </div>
-    )
-}
+  return (
+    <div id="footer">
+      <div id="goBack" onClick={handleGoBack}>
+        Back
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/Components/StudentContainer.js
+++ src/Components/StudentContainer.js
@@ -1,50 +1,56 @@
 import React from "react";
 import StudentCard from "./StudentCard";
-import AddStudentCard from './AddStudentCard';
+import AddStudentCard from "./AddStudentCard";
+import { Route, useHistory } from "react-router-dom";
 
-const StudentContainer = ({ studentData, addNewStudent, cohortIndex, updateStudent, deleteStudent, deleteCohort }) => {
-    
+const StudentContainer = ({
+  studentData,
+  addNewStudent,
+  cohortIndex,
+  updateStudent,
+  deleteStudent,
+  deleteCohort,
+  setCohortIndex,
+}) => {
+  const history = useHistory();
+  const allStudents = studentData.map((studentData) => (
+    <StudentCard
+      key={studentData.id}
+      studentData={studentData}
+      updateStudent={updateStudent}
+      deleteStudent={deleteStudent}
+    />
+  ));
 
-
-    const allStudents = studentData.map((studentData) => (
-        <StudentCard 
-            key={studentData.id} 
-            studentData={studentData} 
-            updateStudent={updateStudent}
-            deleteStudent={deleteStudent}/>
-    ));
-
-    const handleDelete = ()=> {
-        deleteCohort(cohortIndex)
-    }
-
+  const handleDelete = () => {
+    deleteCohort(cohortIndex);
+    setCohortIndex(1);
+    history.push("/");
+  };
 
   return (
     <div>
-        <div id="student-container">
-            <div id="student-list">{allStudents}</div>
-            <AddStudentCard 
-                addNewStudent={addNewStudent} 
-                cohortIndex={cohortIndex}/>
-        </div>
-        <div id="delete-cohort-button" onClick={handleDelete}>
-            <h3>Delete <br />cohort</h3>
-        </div>
+      <div id="student-container">
+        <div id="student-list">{allStudents}</div>
+        <AddStudentCard
+          addNewStudent={addNewStudent}
+          cohortIndex={cohortIndex}
+        />
+      </div>
+      <div id="delete-cohort-button" onClick={handleDelete}>
+        <h3>
+          Delete <br />
+          cohort
+        </h3>
+      </div>
     </div>
-  )     
+  );
 };
 
 export default StudentContainer;
 
-
-
-
-
-
-
 // import React from "react";
 // import StudentCard from "./StudentCard";
-
 
 // const StudentContainer = ({ studentData }) => {
 //   const allStudents = studentData.map((studentData) => (
